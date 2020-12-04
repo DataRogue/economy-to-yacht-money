db49749c51016ec58bed2eeb4fbd0c89b4085830
DataRogue <dmitri@bandannabandit.games>
2020-12-04 12:47:36 -0800

    Init

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Economy\b/g, "Rich people's yacht money");
+	v = v.replace(/\bThe economy\b/g, "Rich people's yacht money");
+	v = v.replace(/\bthe economy\b/g, "rich people's yacht money");
+	v = v.replace(/\bthe Economy\b/g, "rich people's yacht money");
 	
 	textNode.nodeValue = v;
 }
